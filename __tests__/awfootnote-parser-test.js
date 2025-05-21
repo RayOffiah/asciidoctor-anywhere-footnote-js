8f5ea76dfe1f5087d9c8f2658d6fa20d2f340828
Ray Offiah <ray.offiah@couchbase.com>
2025-05-21 09:42:26 +0100

    Work in progress.

--- __tests__/awfootnote-parser-test.js
+++ __tests__/awfootnote-parser-test.js
@@ -30,3 +30,13 @@ test('Test for two footnotes on the same line', () => {
 
 
 })
+
+test('Test for three footnotes on the same line', () => {
+
+    const footnote_list = parser.parse("This is my{empty}awfootnote:my-block[refid='key'] followed by another oneawfootnote:my-block[With text]. And another block.awfootnote:my-block[Spare text]", {actions}).elements[1]
+
+    expect(footnote_list).not.toBeNull()
+    expect(footnote_list.size).toBe(3)
+
+
+})
